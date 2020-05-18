808a7e3d636a9ff3802aa1b9b1915bd68dbec1bf
Đặng Minh Quy <nampk095@gmail.com>
2020-05-18 18:44:43 +0200

    Update and rename functionGetAllTudien.php to functiongetAllTudien.php

---
+++ functiongetAllTudien.php
@@ -0,0 +1,28 @@
+function getAllTudien()
+{
+    // Gọi tới biến toàn cục $conn
+    global $conn;
+    
+    // Hàm kết nối
+    connectDB();
+    
+    // Câu truy vấn lấy tất cả sinh viên
+    $sql = "select * from tudien ORDER BY stt DESC";
+    
+    // Thực hiện câu truy vấn
+    $query = mysqli_query($conn, $sql);
+    
+    // Mảng chứa kết quả
+    $result = array();
+    
+    // Lặp qua từng record và đưa vào biến kết quả
+    if ($query){
+        while ($row = mysqli_fetch_assoc($query)){
+            $result[] = $row;
+        }
+    }
+    
+    // Trả kết quả về
+    return $result;
+}
+
