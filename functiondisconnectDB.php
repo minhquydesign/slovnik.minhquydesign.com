870831f02288b1dfa3184f6274fc2412948ef5b5
Đặng Minh Quy <nampk095@gmail.com>
2020-05-19 19:54:25 +0200

    Create functiondisconnectDB.php

---
+++ functiondisconnectDB.php
@@ -0,0 +1,11 @@
+//// HÀM NGẮT KẾT NỐI DB
+function disconnectDB()
+{
+    // Gọi tới biến toàn cục $conn
+    global $conn;
+    
+    // Nếu đã kêt nối thì thực hiện ngắt kết nối
+    if ($conn){
+        mysqli_close($conn);
+    }
+}
